861df48b64383165464fbaa9198bbbc59d357282
Momina Babar <momina.babar@auxcube.com>
2023-11-01 00:05:34 +0500

    feat: add basi structure

---
+++ src/lib/components/SkillList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Skill } from '$lib/types';
+	import { getAssetURL } from '$lib/data/assets';
+
+	export let items: Array<Skill> = [];
+</script>
+
+<div class="carrousel flex-[0.5] row-center">
+		{#each items as item}
+			<div class="box-content w-150px p-15px col-center">
+				<img class="w-120px h-120px aspect-square" src={getAssetURL(item.logo)} alt={item.name} />
+				<span class="text-center m-t-20px">{item.name}</span>
+			</div>
+		{/each}
+</div>
